cc3c1f00fee1b814d7e95dc26eb329ac2c8bfb28
lelouch187 <vanushs187@gmail.com>
2023-12-12 21:57:34 +0600

    Create WorkerController

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/workers', [\App\Http\Controllers\WorkerController::class, 'index']);
+
